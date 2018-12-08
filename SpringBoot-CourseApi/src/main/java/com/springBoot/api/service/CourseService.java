3aa1fb24070468be7227c6b07b92e354ca1799be
Mudit Raina <Mudit Raina@Microsoft>
2018-12-08 23:31:16 +0530

    new

--- SpringBoot-CourseApi/src/main/java/com/springBoot/api/service/CourseService.java
+++ SpringBoot-CourseApi/src/main/java/com/springBoot/api/service/CourseService.java
@@ -3,10 +3,16 @@
  */
 package com.springBoot.api.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author Mudit Raina
  *
  */
 public class CourseService {
+	
+	@Autowired
+	private CourseService courseService;
+	
 
 }
