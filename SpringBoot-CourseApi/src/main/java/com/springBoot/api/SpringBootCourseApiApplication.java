5385212ea26a159972974cdcc09482951cd4c0d9
Mudit Raina <Mudit Raina@Microsoft>
2018-12-08 23:08:59 +0530

    New Project

---
+++ SpringBoot-CourseApi/src/main/java/com/springBoot/api/SpringBootCourseApiApplication.java
@@ -0,0 +1,12 @@
+package com.springBoot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseApiApplication.class, args);
+	}
+}
